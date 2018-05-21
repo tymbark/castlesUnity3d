2e3e748a1bd878daaa236fae4533be90b2897ef3
tymbark <tymbark.jablkowy@gmail.com>
2018-05-21 20:19:12 +0200

    Added change nickname scene

--- Assets/OptionsController.cs
+++ Assets/OptionsController.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+
+        UpdateNickNameText();
+
+        GameObject.Find("Cancel")
+                  .GetComponent<ClickActionScript>()
+                  .ClickMethod = obj => { SceneLoader.LoadMainMenuScene(); };
+
+        GameObject.Find("Done")
+                  .GetComponent<ClickActionScript>()
+                  .ClickMethod = obj => {
+                      string newNickname = GameObject.Find("TextNewNickname").GetComponent<Text>().text;
+                      DataPersistance.SavePlayerNickName(newNickname);
+                      UpdateNickNameText();
+                  };
+    }
+
+    private static void UpdateNickNameText() {
+        GameObject.Find("CurrentNickName")
+                          .GetComponent<TMPro.TextMeshProUGUI>()
+                  .text = "current nickname: " + DataPersistance.GetPlayerNickName();
+    }
 }
